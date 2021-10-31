d37cf3339b0fafccf007aa402d5988e0b7d5f2c9
Rafael <rafael_breno@outlook.com>
2021-10-31 00:28:43 -0300

    feat: Add IRC connection

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,22 @@
 module Main where
 
+import System.IO
+import qualified Network.Socket as N
 import Lib
 
+twitchURL = "irc.chat.twitch.tv" :: String
+twitchPort = 6667 :: N.PortNumber
+
 main :: IO ()
-main = someFunc
+main = do
+  h <- connectTo twitchURL twitchPort
+  t <- hGetContents h
+  hSetBuffering stdout NoBuffering
+  print t
+
+connectTo :: N.HostName -> N.PortNumber -> IO Handle
+connectTo host port = do
+  addr : _ <- N.getAddrInfo Nothing (Just host) (Just (show port))
+  sock <- N.socket (N.addrFamily addr) (N.addrSocketType addr) (N.addrProtocol addr)
+  N.connect sock (N.addrAddress addr)
+  N.socketToHandle sock ReadWriteMode
