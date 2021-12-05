56579002f3a21830e9b04591c52164a7cecb4f07
Rafael <rafael_breno@outlook.com>
2021-12-05 18:19:13 -0300

    feat: Implement connectTo in Main

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,22 @@
 module Main where
 
 import Lib
+import qualified Network.Socket as N -- network
+import GHC.IO.Handle.Text (hGetContents)
+import System.IO (hGetContents, hSetBuffering, stdout, BufferMode (NoBuffering))
+import qualified IRC
+
+-- IRC URL
+twitchIRC     = "irc.chat.twitch.tv" :: String
+-- IRC Port
+twitchIRCPort = 6667 :: N.PortNumber
+-- Bot's Twitch username
+botUsername   = "rafiuskybot" :: String
+-- Bot's OAuth token
+botOAuthToken = "" :: String
+-- Twitch channel to listen the chat
+twitchChannel = "rafiusky" :: String
 
 main :: IO ()
-main = someFunc
+main = do
+  h <- IRC.connectTo twitchIRC twitchIRCPort
